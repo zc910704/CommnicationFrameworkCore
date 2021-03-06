2e96f59f0089c610a15982145b4b688055f64fdb
Zhang Chen <329188614@qq.com>
2021-03-06 17:28:56 +0800

    新增crc算法

---
+++ Common/Utils/MathUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.HashFunction.CRC;
+
+namespace CommDeviceCore.Common.Utils
+{
+    public static class MathUtils
+    {
+        public static readonly ICRC crc = CRCFactory.Instance.Create(CRCConfig.XMODEM);
+
+        public static UInt16 CalculateCRC16(byte[] data)
+        {
+            var value = crc.ComputeHash(data);
+            var hash = BitConverter.ToUInt16(value.Hash,0);
+            return hash;
+        }
+    }
+}
--- Test/Test.cs
+++ Test/Test.cs
@@ -1,8 +1,25 @@
-﻿using CommDeviceCore.PhysicalCommDevice;
+﻿using CommDeviceCore.PrivateProtocol;
+using CommDeviceCore.PhysicalCommDevice;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using CommDeviceCore.Common.Utils;
+
+namespace CommDeviceCore.PrivateProtocol.Tests
+{
+    public class Test
+    {
+        [Fact()]
+        public void CRC16Test()
+        {
+            byte[] test = new byte[] {0x11, 0x22, 0x33, 0x44 };
+            var crc1 = MathHelper.CRC16(test);
+            var crc2 = MathUtils.CalculateCRC16(test);
+            Assert.True(crc1 == crc2, "This test needs an implementation");
+        }
+    }
+}
 
 namespace CommDeviceCore.PhysicalCommDevice.Tests
 {
@@ -11,7 +28,7 @@ namespace CommDeviceCore.PhysicalCommDevice.Tests
         [Fact()]
         public void OpenTest()
         {
-            Assert.True(false, "This test needs an implementation");
+            Assert.True(true, "This test needs an implementation");
         }
     }
 }
